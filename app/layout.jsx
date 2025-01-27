899caa4957692f4b1f95385ba6d5f490c5aafec5
Changnam Go <changnam.go@gmail.com>
2025-01-27 16:24:22 +0900

    theme provider 를 통해서 theme 변경하는 기능 추가

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,20 @@
+'use client';
+
 // app/layout.js
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import '@/styles/global.css';
+import { ThemeProvider } from '@/context/ThemeContext';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        {children}
-        <Footer />
+        <ThemeProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
+'use client';
+
 // app/page.js
 import Link from 'next/link';
+import { useTheme } from '@/context/ThemeContext';
 
 export default function HomePage() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <main className="container">
+    <main style={{ backgroundColor: theme === 'light' ? '#fff' : '#333', color: theme === 'light' ? '#000' : '#fff' }}>
       <h1>Welcome to Our Store</h1>
       <p>Your one-stop shop for exclusive products.</p>
       <div className="button-group">
@@ -14,6 +19,8 @@ export default function HomePage() {
           <button className="btn-secondary">Browse Collections</button>
         </Link>
       </div>
+      <h1>{theme === 'light' ? 'Light Theme' : 'Dark Theme'}</h1>
+      <button onClick={toggleTheme}>Toggle Theme</button>
     </main>
   );
 }
