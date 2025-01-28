4ab7166a369eaf22bbcc55cc9c2b0f5ec95a6ebd
Changnam Go <changnam.go@gmail.com>
2025-01-28 20:10:11 +0900

    css function 사용하려면 반드시 pragma 부분도 해줘야 함. 상단

--- app/mui/page.jsx
+++ app/mui/page.jsx
@@ -1,9 +1,16 @@
+'use client'
+
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { Button } from "@mui/material";
 
-export default async function Page() {
+export default function Page() {
+    const font = css`
+        text-transform: none;
+    `;
   return (
     <>
-        <Button variant="text" color="primary">Text</Button>
+        <Button variant="text" color="primary" css={font}>Text</Button>
         <Button variant="contained" color="primary">Contained</Button>
         <Button variant="outlined" color="primary">Outlined</Button>
     </>
