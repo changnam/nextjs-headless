9e58c0398cd6dfb2d8a4f567566dab759c97598f
Changnam Go <changnam.go@gmail.com>
2025-01-28 21:01:44 +0900

    mui drawer

--- app/mui/page.jsx
+++ app/mui/page.jsx
@@ -1,16 +1,45 @@
-'use client'
+'use client';
 
-// /** @jsxImportSource @emotion/react */
-// import { css } from "@emotion/react";
-import { Button } from "@mui/material";
+import { useState } from 'react';
+import { Home, Mail, Info, AccountTree } from '@mui/icons-material';
+import { Box, Button, Drawer, List, ListItem, ListItemButton,
+  ListItemText, ListItemIcon } from '@mui/material';
 
-export default function Page() {
+// 표시용 메뉴 정보 준비
+const menu = [
+  { title: '홈', href: 'home.html', icon: Home },
+  { title: 'Contact Us', href: 'contact.html', icon: Mail },
+  { title: '회사 소개', href: 'company.html', icon: Info  },
+  { title: '사이트맵', href: 'sitemap.html', icon: AccountTree },
+];
+
+export default function MaterialDrawer() {
+  // 드로워 개폐를 위한 플래그
+  const [show, setShow] = useState(false);
+  // 버튼 클릭 시 호출되는 핸들러 (show를 반전)
+  const handleDraw = () => setShow(!show);
+
   return (
     <>
-        <Button className="text-red-500" variant="text" color="secondary" >Text</Button>
-        <Button variant="contained" color="primary">Contained</Button>
-        <Button variant="outlined" color="primary">Outlined</Button>
-        <p className="text-red-500">블라블라</p>
+    <Button onClick={handleDraw}>드로워</Button>
+    <Drawer anchor="left" open={show}>
+      <Box sx={{ height: '100vh' }} onClick={handleDraw}>
+      <List>
+      {/* 미리 준비된 배열을 메뉴로 확장 */}
+      {menu.map(obj => {
+        const Icon = obj.icon;
+        return (
+        <ListItem key={obj.title}>
+          <ListItemButton href={obj.href}>
+            <ListItemIcon><Icon /></ListItemIcon>
+            <ListItemText primary={obj.title} />
+          </ListItemButton>
+        </ListItem>
+        );
+      })}
+      </List>
+      </Box>
+    </Drawer>
     </>
   );
 }
