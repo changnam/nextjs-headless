360e2927ca2898443d01ee6a19ddee14b2e73c7b
Changnam Go <changnam.go@gmail.com>
2025-01-28 20:30:35 +0900

    @tailwind 관련 global.css 에 포함되어야 함. text-red-500 처럼 shade 값으로 줘야함. mui 컴포넌트는 tailwind class 가 적용되지 않음

--- app/mui/page.jsx
+++ app/mui/page.jsx
@@ -1,18 +1,16 @@
 'use client'
 
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
+// /** @jsxImportSource @emotion/react */
+// import { css } from "@emotion/react";
 import { Button } from "@mui/material";
 
 export default function Page() {
-    const font = css`
-        text-transform: none;
-    `;
   return (
     <>
-        <Button variant="text" color="primary" css={font}>Text</Button>
+        <Button className="text-red-500" variant="text" color="secondary" >Text</Button>
         <Button variant="contained" color="primary">Contained</Button>
         <Button variant="outlined" color="primary">Outlined</Button>
+        <p className="text-red-500">블라블라</p>
     </>
   );
 }
