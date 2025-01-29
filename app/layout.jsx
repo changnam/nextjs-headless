da8ae87840669b657682aacdd890ae53324e9d08
Changnam Go <changnam.go@gmail.com>
2025-01-30 00:15:01 +0900

    layout 에서 client 컴포넌트 제거

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-'use client';
+// 'use client';
 
 // app/layout.js
 import Navbar from '@/components/Navbar';
@@ -6,30 +6,20 @@ import Footer from '@/components/Footer';
 import './globals.css';
 // import { ThemeProvider } from '@/context/ThemeContext';
 import { ThemeProvider as NextThemeProvider } from 'next-themes';
-import { useEffect, useState } from 'react';
-import ButtonAppBar from '@/components/mui/ButtonAppBar';
-import ResponsiveAppBar from '@/components/mui/ResponsiveAppBar';
-import PrimarySearchAppBar from '@/components/mui/PrimarySearchAppBar';
+// import { useEffect, useState } from 'react';
+// import ButtonAppBar from '@/components/mui/ButtonAppBar';
+// import ResponsiveAppBar from '@/components/mui/ResponsiveAppBar';
+// import PrimarySearchAppBar from '@/components/mui/PrimarySearchAppBar';
 
 export default function RootLayout({ children }) {
-    const [mounted, setMounted] = useState(false)
-
-    // useEffect only runs on the client, so now we can safely show the UI
-    useEffect(() => {
-      setMounted(true)
-    }, [])
-  
-    if (!mounted) {
-      return null
-    }
   
   return (
-    <html lang="en">      
+    <html lang="en" suppressHydrationWarning>      
       <body className="flex flex-col min-h-screen">
         <main className="flex-1 overflow-y-auto">
           <NextThemeProvider attribute="class">
             {/*<ThemeProvider>*/}
-              <ResponsiveAppBar />
+             {/*<ResponsiveAppBar />*/}
               <Navbar />
               {children}
               <Footer />
