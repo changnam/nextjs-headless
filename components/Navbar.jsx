15b77b56162b2394ea601151bb9ff46163489374
Changnam Go <changnam.go@gmail.com>
2025-01-30 00:46:36 +0900

    icon 3개 추가

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,23 @@
 // components/Navbar.js
+'use client'; 
 import Link from 'next/link';
 import Image from 'next/image';
-
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
+import { useState, useEffect } from 'react';
 export default function Navbar() {
+  const [mounted, setMounted] = useState(false)
+
+//  useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+   if (!mounted) {
+    return null
+  }
+
   return (
     <nav className="navbar">
       <Link href="/">
@@ -18,6 +33,9 @@ export default function Navbar() {
         <Link href="/theme">Theme</Link>
         <Link href="/books" className='hidden'>Books</Link>
         <Link href="/reviews" className='hidden'>Reviews</Link>
+        <Link href="#" ><SearchIcon></SearchIcon></Link>
+        <Link href="#" ><PersonOutlineIcon></PersonOutlineIcon></Link>
+        <Link href="#" ><ShoppingCartCheckoutIcon></ShoppingCartCheckoutIcon></Link>
       </div>
     </nav>
   );
