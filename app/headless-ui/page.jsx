410276269ffe11ba78e6119c359604aa9fdebf33
Changnam Go <changnam.go@gmail.com>
2025-01-31 20:54:58 +0900

    menuitem 에 disable 기능

--- app/headless-ui/page.jsx
+++ app/headless-ui/page.jsx
@@ -1,19 +1,14 @@
-'use client';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 
 export default function Example() {
-  function showSettingsDialog() {
-    alert('Open settings dialog!')
-  }
-
   return (
     <Menu>
       <MenuButton>My account</MenuButton>
       <MenuItems anchor="bottom">
         <MenuItem>
-          <button onClick={showSettingsDialog} className="block w-full text-left data-[focus]:bg-blue-100">
+          <a className="block data-[focus]:bg-blue-100" href="/settings">
             Settings
-          </button>
+          </a>
         </MenuItem>
         <MenuItem>
           <a className="block data-[focus]:bg-blue-100" href="/support">
@@ -25,13 +20,11 @@ export default function Example() {
             License
           </a>
         </MenuItem>
-        <form action="/logout" method="post">
-          <MenuItem>
-            <button type="submit" className="block w-full text-left data-[focus]:bg-blue-100">
-              Sign out
-            </button>
-          </MenuItem>
-        </form>
+        <MenuItem disabled>
+          <a className="block data-[disabled]:opacity-50" href="/invite-a-friend">
+            Invite a friend (coming soon!)
+          </a>
+        </MenuItem>
       </MenuItems>
     </Menu>
   )
