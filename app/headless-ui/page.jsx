297c70bd9e82e0173e81d3f21e1c0e2c8d5fc25b
Changnam Go <changnam.go@gmail.com>
2025-01-31 20:45:42 +0900

    attribute 를 이용해서 styling

--- app/headless-ui/page.jsx
+++ app/headless-ui/page.jsx
@@ -1,25 +1,21 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 
+const links = [
+    { href: '/settings', label: 'Settings' },
+    { href: '/support', label: 'Support' },
+    { href: '/license', label: 'License' },
+  ]
+
 export default function MyMenu() {
   return (
     <Menu>
-      <MenuButton>My account</MenuButton>
+      <MenuButton className="data-[active]:bg-blue-200">My account</MenuButton>
       <MenuItems anchor="bottom">
-        <MenuItem>
-          <a className="block data-[focus]:bg-blue-100" href="/settings">
-            Settings
-          </a>
-        </MenuItem>
-        <MenuItem>
-          <a className="block data-[focus]:bg-blue-100" href="/support">
-            Support
-          </a>
-        </MenuItem>
-        <MenuItem>
-          <a className="block data-[focus]:bg-blue-100" href="/license">
-            License
-          </a>
-        </MenuItem>
+        {links.map((link) => (
+          <MenuItem key={link.href} className="block data-[focus]:bg-blue-200">
+            <a href={link.href}>{link.label}</a>
+          </MenuItem>
+        ))}
       </MenuItems>
     </Menu>
   )
