497799ade23882592360fadeab1b9eed299b7bf0
Changnam Go <changnam.go@gmail.com>
2025-01-31 20:52:25 +0900

    menu click 시 링크가 아닌 버튼으로 동작

--- app/headless-ui/page.jsx
+++ app/headless-ui/page.jsx
@@ -1,21 +1,37 @@
+'use client';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 
-const links = [
-    { href: '/settings', label: 'Settings' },
-    { href: '/support', label: 'Support' },
-    { href: '/license', label: 'License' },
-  ]
+export default function Example() {
+  function showSettingsDialog() {
+    alert('Open settings dialog!')
+  }
 
-export default function MyMenu() {
   return (
     <Menu>
-      <MenuButton className="data-[active]:bg-blue-200">My account</MenuButton>
+      <MenuButton>My account</MenuButton>
       <MenuItems anchor="bottom">
-        {links.map((link) => (
-          <MenuItem key={link.href} className="block data-[focus]:bg-blue-200">
-            <a href={link.href}>{link.label}</a>
+        <MenuItem>
+          <button onClick={showSettingsDialog} className="block w-full text-left data-[focus]:bg-blue-100">
+            Settings
+          </button>
+        </MenuItem>
+        <MenuItem>
+          <a className="block data-[focus]:bg-blue-100" href="/support">
+            Support
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a className="block data-[focus]:bg-blue-100" href="/license">
+            License
+          </a>
+        </MenuItem>
+        <form action="/logout" method="post">
+          <MenuItem>
+            <button type="submit" className="block w-full text-left data-[focus]:bg-blue-100">
+              Sign out
+            </button>
           </MenuItem>
-        ))}
+        </form>
       </MenuItems>
     </Menu>
   )
