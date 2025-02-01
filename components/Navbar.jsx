36d3ca5f007b44fab105cf435f65dc1a875c8e16
Changnam Go <changnam.go@gmail.com>
2025-02-01 09:17:21 +0900

    hamburger icon 추가, className invert 로 색깔, 배경색 바꾸기 가능

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,6 +6,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 import { useState, useEffect } from 'react';
+import Hamburger from '@/components/Hamburger';
 export default function Navbar() {
   const [mounted, setMounted] = useState(false)
 
@@ -20,7 +21,7 @@ export default function Navbar() {
 
   return (
     <nav className="flex items-center justify-between p-2 bg-gray-800 text-white">
-      <Link href="/" className="flex-none items-start gap-2">
+      <Link href="/" className="flex-none items-start gap-2 hidden md:block">
         {/*<Image src="/images/logo.png" alt="Logo" width={100} height={50} />*/}
         <svg width="150" height="30" xmlns="http://www.w3.org/2000/svg">
           {/*<rect width="100%" height="100%" fill="gray" />*/}
@@ -28,6 +29,16 @@ export default function Navbar() {
           <text x="75" y="30" fontSize="30" textAnchor="middle" fill="white">BlackMotiv</text>
         </svg>
       </Link>
+      <Link href="#" className="block md:hidden">
+      <Image
+          className="invert dark:invert"
+          src="/hamburger-md-svgrepo-com.svg"
+          alt="Hamburger menu"
+          width={50}
+          height={38}
+          priority
+        />
+        </Link>
       <div className="hidden md:block md:mx-auto">
         <Link href="/">Home</Link>
         <Link href="/products">Products</Link>
@@ -41,6 +52,7 @@ export default function Navbar() {
         <Link href="/popover" >Popover</Link>
         <Link href="/navbar" >ReactNavbar</Link>
         <Link href="/headless-ui" >HeadlessUI</Link>
+        {/*<Link href="#" ></Link>*/}
         <Link href="#" ><SearchIcon></SearchIcon></Link>
         <Link href="#" ><PersonOutlineIcon></PersonOutlineIcon></Link>
         <Link href="#" ><ShoppingCartCheckoutIcon></ShoppingCartCheckoutIcon></Link>
